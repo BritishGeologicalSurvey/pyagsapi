51d0be1ce7ef523f72dc63acbc8fce32fabd2e15
KoalaGeo <eddlewis85@gmail.com>
2021-08-16 19:40:43 +0100

    init

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from typing import List
 
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Request, Form
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
 
 @app.post("/check/")
 async def check(files: List[bytes] = File(...)):
@@ -23,29 +28,9 @@ async def create_upload_files(files: List[UploadFile] = File(...)):
     return {"filenames": [file.filename for file in files]}
 
 
-@app.get("/")
-async def main():
-    content = """
-<!DOCTYPE html>
-<html>
-<head>
-<title>AGS File Validator</title>
-</head>
-<body>
-<h1>AGS File Validator</h1>
-<br>
-<h2>AGS File Size</h2>
-<form action="/files/" enctype="multipart/form-data" method="post">
-<input name="files" type="file" multiple>
-<input type="submit">
-</form>
-<br>
-<h2>AGS File Name</h2>
-<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
-<input name="files" type="file" multiple>
-<input type="submit">
-</form>
-</body>
-</html>
-    """
-    return HTMLResponse(content=content)                                                                                                               
+@app.get("/", response_class=HTMLResponse)
+async def homepage(request):
+    return templates.TemplateResponse('index.html', {'request': request})   
+
+
+]                                                                                                           
