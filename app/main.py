a93b515f87ebeb97b65aa77eee55395f6003f080
Colin Blackburn <colb@bgs.ac.uk>
2021-08-16 16:28:23 +0100

    Very basic validate endpoint

--- app/main.py
+++ app/main.py
@@ -1,21 +1,25 @@
+import tempfile
+
+from pathlib import Path
 from typing import List
 
 from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
+
+import ags
+
 
 app = FastAPI()
 
 
-@app.post("/check/")
-async def check(files: List[bytes] = File(...)):
-    # Create temp dir
-    # Loop over files
-    #   - save to temp dir
-    #   - call validate (and save log)
-    # Zip up log files
-    # Return zip
-    # Errors should be 4xx if invalid file is uploaded
-    return {"file_sizes": [len(file) for file in files]}
+@app.post("/validate/", response_class=FileResponse)
+async def validate(file: UploadFile = File(...)):
+    tmp_dir = Path(tempfile.mkdtemp())
+    contents = await file.read()
+    local_ags_file = tmp_dir / file.filename
+    local_ags_file.write_bytes(contents)
+    logfile = ags.validate(local_ags_file, tmp_dir)
+    return logfile
 
 
 @app.post("/uploadfiles/")
@@ -35,8 +39,8 @@ async def main():
 <h1>AGS File Validator</h1>
 <br>
 <h2>AGS File Size</h2>
-<form action="/files/" enctype="multipart/form-data" method="post">
-<input name="files" type="file" multiple>
+<form action="/validate/" enctype="multipart/form-data" method="post">
+<input name="file" type="file" multiple>
 <input type="submit">
 </form>
 <br>
@@ -48,4 +52,4 @@ async def main():
 </body>
 </html>
     """
-    return HTMLResponse(content=content)                                                                                                               
+    return HTMLResponse(content=content)
