9b1b059edbaef2b8ab98a9ac173f2ef2c175c001
Colin Blackburn <colb@bgs.ac.uk>
2021-08-17 10:57:40 +0100

    Add background task to rm tmp files

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 import tempfile
+import shutil
 
 from pathlib import Path
 from typing import List
 
-from fastapi import APIRouter, File, UploadFile
+from fastapi import APIRouter, BackgroundTasks, File, UploadFile
 from fastapi.responses import FileResponse
 
 from app import ags
@@ -13,8 +14,10 @@ router = APIRouter()
 
 
 @router.post("/validate/", response_class=FileResponse)
-async def validate(file: UploadFile = File(...)):
+async def validate(background_tasks: BackgroundTasks,
+                   file: UploadFile = File(...)):
     tmp_dir = Path(tempfile.mkdtemp())
+    background_tasks.add_task(shutil.rmtree, tmp_dir)
     contents = await file.read()
     local_ags_file = tmp_dir / file.filename
     local_ags_file.write_bytes(contents)
@@ -23,12 +26,17 @@ async def validate(file: UploadFile = File(...)):
 
 
 @router.post("/validatemany/", response_class=FileResponse)
-async def validate_many(files: List[UploadFile] = File(...)):
+async def validate_many(background_tasks: BackgroundTasks,
+                        files: List[UploadFile] = File(...)):
     tmp_dir = Path(tempfile.mkdtemp())
+    background_tasks.add_task(shutil.rmtree, tmp_dir)
     full_logfile = tmp_dir / 'logfile.log'
     with full_logfile.open('wt') as f:
         for file in files:
-            logfile = await validate(file)
+            contents = await file.read()
+            local_ags_file = tmp_dir / file.filename
+            local_ags_file.write_bytes(contents)
+            logfile = ags.validate(local_ags_file, tmp_dir)
             f.write(logfile.read_text())
             f.write('=' * 80 + '\n')
     return full_logfile
