80d69051c51b5487b0a7d00128278aaac400dd99
John A Stevenson <jostev@bgs.ac.uk>
2021-08-17 14:29:40 +0100

    Populate convert method

--- app/ags.py
+++ app/ags.py
@@ -9,8 +9,8 @@ logger = logging.getLogger(__name__)
 LOGFILE_TEMPLATE = dedent("""
     File Name: \t {filename}
 
-    {error_message}
-    """.strip())
+    {message}
+    """).strip()
 
 
 def validate(filename: Path, results_dir: Path) -> Path:
@@ -34,20 +34,52 @@ def validate(filename: Path, results_dir: Path) -> Path:
     # Look for errors in the results
     error = None
     if result.returncode != 0:
-        error = result.stderr
+        error = 'ERROR: ' + result.stderr
     if result.stdout.startswith('ERROR'):
         error = result.stdout
 
     if error:
-        contents = LOGFILE_TEMPLATE.format(filename=filename.name, error_message=error)
+        contents = LOGFILE_TEMPLATE.format(filename=filename.name, message=error)
         logfile.write_text(contents)
 
     return logfile
 
 
 def convert(filename, results_dir):
+    """Validate filename and write output to file in results_dir."""
     # Call ags4_cli to convert file
-    pass
+    if not results_dir.exists():
+        results_dir.mkdir()
+
+    new_extension = '.ags' if filename.suffix == '.xlsx' else '.xlsx'
+    converted_file = results_dir / (filename.stem + new_extension)
+    logfile = results_dir / (filename.name + '.log')
+
+    args = [
+        'ags4_cli', 'convert', filename, converted_file
+    ]
+    # Use subprocess to run the file.  It will swallow errors.
+    # If files have repeated headers the CLI will ask if they should be
+    # renamed.  Passing input='n' answers 'n' to this question.
+    # A timeout prevents the whole process hanging indefinitely.
+    result = subprocess.run(args, capture_output=True,
+                            input='n', text=True, timeout=30)
+    logger.debug(result)
+
+    # Prepare log file content
+    if result.returncode != 0:
+        message = 'ERROR: ' + result.stderr
+        converted_file = None
+    elif result.stdout.startswith('ERROR'):
+        message = result.stdout
+        converted_file = None
+    else:
+        message = f"SUCCESS: {filename.name} converted to {converted_file.name}"
+
+    contents = LOGFILE_TEMPLATE.format(filename=filename.name, message=message)
+    logfile.write_text(contents)
+
+    return (converted_file, logfile)
 
 
 class Ags4CliError(Exception):
--- test/unit/test_ags.py
+++ test/unit/test_ags.py
@@ -48,3 +48,46 @@ def test_validate_unreadable_files(tmp_path, filename, expected):
     output = logfile.read_text()
     assert f"File Name: \t {filename.name}" in output
     assert re.search(expected, output)
+
+
+@pytest.mark.parametrize('filename, expected', [
+    ('example1.ags', 'SUCCESS: example1.ags converted to example1.xlsx'),
+    ('example1.xlsx', 'SUCCESS: example1.xlsx converted to example1.ags'),
+])
+def test_convert(tmp_path, filename, expected):
+    # Arrange
+    filename = TEST_FILE_DIR / filename
+    results_dir = tmp_path / 'results'
+    if not results_dir.exists:
+        results_dir.mkdir()
+
+    # Act
+    converted_file, logfile = ags.convert(filename, results_dir)
+
+    # Assert
+    output = logfile.read_text()
+    assert converted_file is not None and converted_file.exists()
+    assert expected in output
+
+
+@pytest.mark.parametrize('filename, expected', [
+    ('nonsense.ags', 'IndexError: At least one sheet must be visible'),
+    ('empty.ags', 'IndexError: At least one sheet must be visible'),
+    ('dummy.xlsx', "AttributeError: 'DataFrame' object has no attribute 'HEADING'"),
+    ('random_binary.ags', 'IndexError: At least one sheet must be visible'),
+])
+def test_convert_bad_files(tmp_path, filename, expected):
+    # Arrange
+    filename = TEST_FILE_DIR / filename
+    results_dir = tmp_path / 'results'
+    if not results_dir.exists:
+        results_dir.mkdir()
+
+    # Act
+    converted_file, logfile = ags.convert(filename, results_dir)
+
+    # Assert
+    output = logfile.read_text()
+    assert converted_file is None
+    assert 'ERROR:' in output
+    assert re.search(expected, output)
