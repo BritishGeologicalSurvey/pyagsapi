16ae5f06d349855c55bb35877868c143f4d55765
Colin Blackburn <colb@bgs.ac.uk>
2021-08-25 13:55:56 +0100

    Add HTML response with simple paragraphs

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from typing import List
 
 from fastapi import APIRouter, BackgroundTasks, File, Form, Request, UploadFile
-from fastapi.responses import FileResponse, StreamingResponse
+from fastapi.responses import FileResponse, HTMLResponse, StreamingResponse
 
 from app import ags
 from app.errors import error_responses, InvalidPayloadError
@@ -16,8 +16,8 @@ router = APIRouter()
 
 log_responses = dict(error_responses)
 log_responses['200'] = {
-    "content": {"text/plain": {}},
-    "description": "Return a log file"}
+    "content": {"text/plain": {}, "text/html": {}},
+    "description": "Return a log in json, text or html"}
 
 zip_responses = dict(error_responses)
 zip_responses['200'] = {
@@ -29,6 +29,7 @@ zip_responses['200'] = {
 class Format(str, Enum):
     TEXT = "text"
     JSON = "json"
+    HTML = "html"
 
 
 # Enum for search logic
@@ -41,7 +42,7 @@ class Dictionary(str, Enum):
 format_form = Form(
     default=Format.JSON,
     title='Response Format',
-    description='Response format: json or text',
+    description='Response format: json, text or html',
 )
 
 dictionary_form = Form(
@@ -110,6 +111,10 @@ async def validate(background_tasks: BackgroundTasks,
         logfile = tmp_dir / 'results.log'
         logfile.write_text(log)
         response = FileResponse(logfile, media_type="text/plain")
+    elif fmt == Format.HTML:
+        log = ags.to_plain_text(result)
+        html = '<p>' + log + '</p>'
+        response = HTMLResponse(html, media_type="text/html")
     else:
         data = [result]
         response = prepare_validation_response(request, data)
@@ -143,6 +148,16 @@ async def validate_many(background_tasks: BackgroundTasks,
                 f.write(log)
                 f.write('=' * 80 + '\n')
         response = FileResponse(full_logfile, media_type="text/plain")
+    elif fmt == Format.HTML:
+        html = ''
+        for file in files:
+            contents = await file.read()
+            local_ags_file = tmp_dir / file.filename
+            local_ags_file.write_bytes(contents)
+            result = ags.validate(local_ags_file, standard_AGS4_dictionary=dictionary)
+            log = ags.to_plain_text(result)
+            html = html + '<p>' + log + '</p>\n'
+        response = HTMLResponse(html, media_type="text/html")
     else:
         data = []
         for file in files:
