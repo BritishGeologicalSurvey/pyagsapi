616b9d9d00f432134cbef90bb599ee6509e535b9
Colin Blackburn <colb@bgs.ac.uk>
2021-08-26 11:29:25 +0100

    Test for basic response message

--- test/unit/test_bgs.py
+++ test/unit/test_bgs.py
@@ -1,14 +1,24 @@
 """Tests for calls to AGS functions."""
 from pathlib import Path
 
+import pytest
+
 from app import bgs
 
 TEST_FILE_DIR = Path(__file__).parent.parent / 'files'
 
+FILE_DATA = {
+    'example_ags.ags': ('All checks passed!', True),
+    'extension_is.bad': ('ERROR: extension_is.bad is not .ags format', False),
+}
 
-def test_validate():
+
+@pytest.mark.parametrize('filename, expected',
+                         [item for item in FILE_DATA.items()])
+def test_validate(filename, expected):
     # Arrange
-    filename = TEST_FILE_DIR / 'example_ags.ags'
+    filename = TEST_FILE_DIR / filename
+    expected_message, expected_valid = expected
 
     # Act
     response = bgs.validate(filename)
@@ -17,22 +27,28 @@ def test_validate():
     # Check that metadata fields are correct
     keys = {'filename', 'filesize', 'time', 'errors', 'message', 'valid'}
     assert set(response.keys()) == keys
+    assert response['message'] == expected_message
+    assert response['valid'] == expected_valid
 
 
-def test_is_valid():
+@pytest.mark.parametrize('filename, expected',
+                         [item for item in FILE_DATA.items()])
+def test_is_valid(filename, expected):
     # Arrange
-    filename = TEST_FILE_DIR / 'example_ags.ags'
+    filename = TEST_FILE_DIR / filename
+    expected_valid = expected[1]
 
     # Act
     result = bgs.is_valid(filename)
 
     # Assert
-    assert result
+    assert result == expected_valid
 
 
-def test_to_plain_text():
+@pytest.mark.parametrize('filename', FILE_DATA.keys())
+def test_to_plain_text(filename):
     # Arrange
-    filename = TEST_FILE_DIR / 'example_ags.ags'
+    filename = TEST_FILE_DIR / filename
 
     # Act
     response = bgs.validate(filename)
