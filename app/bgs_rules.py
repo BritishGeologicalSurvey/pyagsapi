44fbe892e31aaa728c9759bd2157c1c286c73c3d
John A Stevenson <jostev@bgs.ac.uk>
2021-08-27 11:43:01 +0100

    Minor refactoring for style

--- app/bgs_rules.py
+++ app/bgs_rules.py
@@ -5,17 +5,17 @@ from python_ags4 import AGS4
 def check_required_groups(tables: dict) -> list:
     """ Groups must include PROJ, LOCA or HOLE, ABBR, TYPE, UNIT """
     errors = []
-    desc = ''
     required = ['PROJ', 'ABBR', 'TYPE', 'UNIT']
+    missing = []
+
     for group in required:
         if group not in tables.keys():
-            desc += group + ', '
+            missing.append(group)
     if 'LOCA' not in tables.keys() and 'HOLE' not in tables.keys():
-        desc += '(LOCA or HOLE)' + ', '
-    if desc:
-        desc = 'Required groups not present: ' + desc
-        desc = desc.rstrip(', ')
-    if desc:
+        missing.append('(LOCA or HOLE)')
+
+    if missing:
+        desc = 'Required groups not present: ' + ', '.join(missing)
         errors.append({'line': '-', 'group': '', 'desc': desc})
 
     return errors
@@ -24,11 +24,15 @@ def check_required_groups(tables: dict) -> list:
 def check_required_bgs_groups(tables: dict) -> list:
     """ Groups must include GEOL for BGS """
     errors = []
-    desc = ''
-    group = 'GEOL'
-    if group not in tables.keys():
-        desc += f'Required BGS groups not present: {group}'
-    if desc:
+    required = ['GEOL']
+    missing = []
+
+    for group in required:
+        if group not in tables.keys():
+            missing.append(group)
+
+    if missing:
+        desc = 'Required BGS groups not present: ' + ', '.join(missing)
         errors.append({'line': '-', 'group': '', 'desc': desc})
 
     return errors
@@ -37,21 +41,23 @@ def check_required_bgs_groups(tables: dict) -> list:
 def check_spatial_referencing_system(tables: dict) -> list:
     """ Spatial referencing system defined in LOCA_GREF, LOCA_LREF or LOCA_LLZ """
     errors = []
+    ref_found = False
+    loca = AGS4.convert_to_numeric(tables['LOCA'])
+
     try:
-        ref_found = False
-        loca = AGS4.convert_to_numeric(tables['LOCA'])
         for col in ['LOCA_GREF', 'LOCA_LREF', 'LOCA_LLZ']:
             try:
                 if all(loca[col] != ''):
                     ref_found = True
             except KeyError:
                 pass
-        if not ref_found:
-            desc = 'Spatial referencing system not in LOCA_GREF, LOCA_LREF or LOCA_LLZ!'
-            errors.append({'line': '-', 'group': 'LOCA', 'desc': desc})
     except KeyError:
-        # LOCA not present, already checked in earlier rule
+        # LOCA not present, already checked in check_required_groups()
         pass
+
+    if not ref_found:
+        desc = 'Spatial referencing system not in LOCA_GREF, LOCA_LREF or LOCA_LLZ!'
+        errors.append({'line': '-', 'group': 'LOCA', 'desc': desc})
 
     return errors
 
