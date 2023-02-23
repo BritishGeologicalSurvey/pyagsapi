98f4d19b8c7b3d402fa190f85baea7914e1be9a1
Colin Blackburn <colb@bgs.ac.uk>
2023-02-23 17:20:59 +0000

    Add sort_tables to convert with tests

--- test/unit/test_conversion.py
+++ test/unit/test_conversion.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import re
 
 import pytest
+import pandas as pd
+from python_ags4 import AGS4
 
 from app.conversion import convert
 from test.fixtures import (BAD_FILE_DATA, GOOD_FILE_DATA)
@@ -26,6 +28,30 @@ def test_convert(tmp_path, filename, expected):
     assert converted_file is not None and converted_file.exists()
     assert response['filename'] == filename.name
     assert re.search(expected_message, response['message'])
+
+
+@pytest.mark.parametrize('sort_tables', [True, False])
+def test_sort_tables(tmp_path, sort_tables):
+    # Arrange
+    filename = Path(__file__).parent.parent / 'files' / 'example_ags.ags'
+    tables, headings = AGS4.AGS4_to_dataframe(filename)
+    groups = list(tables.keys())
+    results_dir = tmp_path / 'results'
+    if not results_dir.exists:
+        results_dir.mkdir()
+
+    # Act
+    converted_file, response = convert(filename, results_dir, sort_tables=sort_tables)
+
+    # Assert
+    assert converted_file is not None and converted_file.exists()
+    assert response['filename'] == filename.name
+
+    xl = pd.ExcelFile(converted_file)
+    if sort_tables:
+        assert xl.sheet_names == sorted(groups)
+    else:
+        assert xl.sheet_names == groups
 
 
 @pytest.mark.parametrize('filename, expected', BAD_FILE_DATA)
