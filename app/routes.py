313acfe51972c034d9056ec50fb4bff4d3c9161f
KoalaGeo <eddlewis85@gmail.com>
2023-04-14 09:44:36 +0100

    Use bgs_loca_id in output filename

--- app/routes.py
+++ app/routes.py
@@ -193,7 +193,8 @@ async def get_ags_log(bgs_loca_id: int):
     url = f"https://webservices.bgs.ac.uk/GWBV/viewborehole?loca_id={bgs_loca_id}"
     response = requests.get(url)
     if response.status_code == 200:
-        headers = {'Content-Disposition': 'attachment; filename="boreholelog.pdf"'}
+        filename = f"{bgs_loca_id}_log.pdf"
+        headers = {'Content-Disposition': f'attachment; filename="{filename}"'}
         return Response(response.content, headers=headers, media_type='application/pdf')
     else:
         return {"error": f"Failed to retrieve borehole {bgs_loca_id}"}
