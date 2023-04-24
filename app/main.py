3a37026c25d61ad1d012e0364e7e936c96dcd5c5
KoalaGeo <eddlewis85@gmail.com>
2023-04-24 15:51:54 +0100

    Add openapi_tags to FastAPI constructor

--- app/main.py
+++ app/main.py
@@ -62,7 +62,7 @@ def setup_logging(logging_level=logging.INFO):
         f"'logging_level': {logging.getLevelName(logging_level)}")
 
 
-app = FastAPI(root_path=os.getenv('PYAGSAPI_ROOT_PATH', ''))
+app = FastAPI(root_path=os.getenv('PYAGSAPI_ROOT_PATH', ''), openapi_tags=tags_metadata)
 
 setup_logging()
 
@@ -76,34 +76,6 @@ app.mount("/static", StaticFiles(directory="app/static"), name="static")
 @app.get("/", response_class=HTMLResponse, include_in_schema=False)
 async def landing_page(request: Request):
     return templates.TemplateResponse('landing_page.html', {'request': request})
-
-
-def custom_openapi():
-    if app.openapi_schema:
-        return app.openapi_schema
-    openapi_schema = get_openapi(
-        title="pyagsapi - AGS File Utilities Tools and API",
-        version="4.5.0",
-        description=("The API performs schema validation, data validation and conversion of your AGS files. It also exports a graphical log from AGS data held by NGDC"
-                     "Schema validation and conversion uses https://gitlab.com/ags-data-format-wg/ags-python-library"),
-        terms_of_service="https://www.bgs.ac.uk/legal-and-policy/terms-of-use/",
-        contact={
-            "name": "BGS Enquiries",
-            "url": "https://www.bgs.ac.uk/about-bgs/contact-us/",
-            "email": "enquiries@bgs.ac.uk",
-        },
-        license_info={
-            "name": "Open Government Licence v3",
-            "url": "https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/",
-        },             
-        routes=app.routes,
-    )
-    openapi_schema["info"]["x-logo"] = {
-        "url": ("https://raw.githubusercontent.com/BritishGeologicalSurvey/pyagsapi"
-                "/main/app/static/img/BGS-Logo-Pos-RGB-01.png")
-    }
-    app.openapi_schema = openapi_schema
-    return app.openapi_schema
 
 tags_metadata = [
     {
@@ -131,6 +103,33 @@ tags_metadata = [
         },
     },
 ]
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="pyagsapi - AGS File Utilities Tools and API",
+        version="4.5.0",
+        description=("The API performs schema validation, data validation and conversion of your AGS files. It also exports a graphical log from AGS data held by NGDC"
+                     "Schema validation and conversion uses https://gitlab.com/ags-data-format-wg/ags-python-library"),
+        terms_of_service="https://www.bgs.ac.uk/legal-and-policy/terms-of-use/",
+        contact={
+            "name": "BGS Enquiries",
+            "url": "https://www.bgs.ac.uk/about-bgs/contact-us/",
+            "email": "enquiries@bgs.ac.uk",
+        },
+        license_info={
+            "name": "Open Government Licence v3",
+            "url": "https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/",
+        },             
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": ("https://raw.githubusercontent.com/BritishGeologicalSurvey/pyagsapi"
+                "/main/app/static/img/BGS-Logo-Pos-RGB-01.png")
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
 
 app.openapi = custom_openapi
 
