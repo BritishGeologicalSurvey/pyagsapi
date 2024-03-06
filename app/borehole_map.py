91d2e8988c90215f1bd75757975001b1fdcaff96
John A Stevenson <jostev@bgs.ac.uk>
2024-03-06 16:11:00 +0000

    Add extract_geojson function

---
+++ app/borehole_map.py
@@ -0,0 +1,57 @@
+"""
+Functions used to generate a map of borehole locations by extracting a GeoJSON
+representation of their metadata from the AGS files.
+"""
+import json
+import logging
+from pathlib import Path
+
+import pandas as pd
+import geopandas as gpd
+
+from app.checkers import load_tables_reporting_errors
+from app.bgs_rules import create_location_gpd
+
+logger = logging.getLogger(__name__)
+
+
+def extract_geojson(filepath: Path) -> dict:
+    """
+    Read an AGS4 file and extract geojson represenation of LOCA table and
+    metadata. 
+    """
+    logger.info("Extracting geojson from  %s", filepath.name)
+
+    # Read data file
+    tables, load_error, _ = load_tables_reporting_errors(filepath)
+    if load_error:
+        raise ValueError(load_error)
+
+    # Convert to geodataframe
+    try:
+        location: gpd.GeoDataFrame = create_location_gpd(tables)
+    except KeyError:
+        msg = f"LOCA group missing from {filepath}"
+        raise ValueError(msg)
+    
+    # Add project columns
+    try:
+        project: pd.DataFrame = tables['PROJ']
+    except KeyError:
+        msg = f"PROJ group missing from {filepath}"
+        raise ValueError(msg)
+
+    for column in project.columns:
+        if column.startswith('PROJ_'):
+            location[column] = project.loc[0, column]
+
+    # Create new ID from project and location IDs
+    location.reset_index(inplace=True)
+    location['ID'] = location['PROJ_ID'].str.cat(location['LOCA_ID'], sep='.')
+    location.set_index('ID', inplace=True)
+
+    # Reproject to WGS84
+    location = location.to_crs('EPSG:4326')
+    
+    # Return dict representation of geojson
+    return json.loads(location.to_json())
---
+++ test/unit/test_borehole_map.py
@@ -0,0 +1,31 @@
+"""
+Tests for borehole_map.py
+"""
+from pathlib import Path
+
+from geojson_pydantic import FeatureCollection
+
+from app.borehole_map import extract_geojson
+
+TEST_FILE_DIR = Path(__file__).parent.parent / 'files'
+
+
+def test_extract_geojson_example_ags():
+    # Arrange
+    filepath = TEST_FILE_DIR / 'example_ags.ags'
+
+    # Act
+    result = extract_geojson(filepath)
+
+    # Assert
+    # Creation of FeatureCollection ensures correct fields exist
+    feature_collection = FeatureCollection(**result)
+    assert len(feature_collection) == 1
+
+    feature = feature_collection[0]
+    assert feature.properties['PROJ_ID'] == '121415'
+    assert feature.properties['LOCA_ID'] == '327-16A'
+    assert feature.id == '121415.327-16A'
+    lon, lat = feature.geometry.coordinates
+    assert -180 <= lon <= 180
+    assert -90 <= lat <= 90
