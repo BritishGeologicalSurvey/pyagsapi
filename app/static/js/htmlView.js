4ce8aa0acd6d5e34412e2873aa251f3b1703bb3b
KoalaGeo <eddlewis85@gmail.com>
2024-03-11 13:52:43 +0000

    Update summary error message

--- app/static/js/htmlView.js
+++ app/static/js/htmlView.js
@@ -192,7 +192,7 @@ agsHtml.displayFileResult=function(fileResult){
         xhtml=xhtml + "<ul class='fileResSummary'>";
 
         if(summaries.length === 0){
-            xhtml=xhtml + "<li>No summary generated due to errors reading file (see below) or BGS validation not selected (required for summary)</li>";
+            xhtml=xhtml + "<li>No summary generated due to errors reading file (see below)</li>";
             };
 
         if(fileResult.additional_metadata.bgs_all_groups){
