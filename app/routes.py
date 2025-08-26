b7b999e7ec5af31228e6c786f7aeeca7b1b7222b
Colin Blackburn <colb@bgs.ac.uk>
2025-08-26 13:21:41 +0100

    Move queries to model queries

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,7 @@ import os
 from pathlib import Path
 from typing import List
 
-from fastapi import APIRouter, BackgroundTasks, File, Form, Query, Request, UploadFile, Response
+from fastapi import APIRouter, BackgroundTasks, Request, UploadFile, Response
 from fastapi.responses import FileResponse, StreamingResponse
 from fastapi.exceptions import HTTPException
 
@@ -20,6 +20,9 @@ from app.checkers import check_ags, check_bgs
 from app.errors import error_responses, InvalidPayloadError
 from app.model.schema import (Checker, Format, Dictionary, SortingStrategy, ResponseType,
                               ValidationResponse, BoreholeCountResponse)
+from app.model.queries import (format_form, geometry_form, dictionary_form, validate_form, sort_tables_form,
+                               validation_file, conversion_file, ags_log_query, ags_export_query,
+                               polygon_query, count_only_query, response_type_query)
 
 BOREHOLE_EXPORT_LIMIT = 50
 BOREHOLE_VIEWER_URL = "https://gwbv.bgs.ac.uk/GWBV/viewborehole?loca_id={bgs_loca_id}"
@@ -58,84 +61,6 @@ checker_functions = {
     Checker.ags: check_ags,
     Checker.bgs: check_bgs,
 }
-
-format_form = Form(
-    default=Format.JSON,
-    title='Response Format',
-    description='Response format: json or text',
-)
-
-geometry_form = Form(
-    default=False,
-    title='GeoJSON Option',
-    description=('Return GeoJSON if possible, otherwise return an error message '
-                 ' Option: True or False'),
-)
-
-dictionary_form = Form(
-    default=Dictionary.None_Given,
-    title='Validation Dictionary',
-    description='Version of AGS dictionary to validate against',
-)
-
-validate_form = Form(
-    default=[Checker.ags],
-    title='Validation Options',
-    description='If set validate against AGS schema',
-)
-
-validation_file = File(
-    ...,
-    title='File to validate',
-    description='An AGS file ending in .ags',
-)
-
-conversion_file = File(
-    ...,
-    title='File to convert',
-    description='An AGS or XLSX file',
-)
-
-sort_tables_form = Form(
-    default=SortingStrategy.default,
-    title='Sort worksheets',
-    description=('Sort the worksheets into alphabetical, hierarchical '
-                 'dictionary or default order, that found in the AGS file. '
-                 'This option is ignored when converting to AGS.'),
-)
-
-ags_log_query = Query(
-    ...,
-    title="BGS LOCA ID",
-    description="BGS LOCA ID",
-    example="20190430093402523419",
-)
-
-ags_export_query = Query(
-    ...,
-    title="BGS LOCA ID",
-    description="A single ID or multiple IDs separated by semicolons",
-    example="20190430093402523419",
-)
-
-polygon_query = Query(
-    ...,
-    title="POLYGON",
-    description="A polygon expressed in Well Known Text",
-    example="POLYGON((-4.5 56,-4 56,-4 55.5,-4.5 55.5,-4.5 56))",
-)
-
-count_only_query = Query(
-    default=False,
-    title='Return count only',
-    description='Return count of found boreholes only',
-)
-
-response_type_query = Query(
-    default=ResponseType.inline,
-    title='PDF Response Type',
-    description='PDF response type: inline or attachment',
-)
 
 
 @router.post("/validate/",
